1de5918f529f16c64914cf53040091852fec29ff
Hande Bengü <hande.bengu@chasacademy.se>
2026-04-06 20:40:24 +0200

    feature: setup Service B (LLM Proxy) with HuggingFace typed client

---
+++ LLMProxyAPI/Program.cs
@@ -0,0 +1,38 @@
+using LLMProxyAPI.Services;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+var huggingFaceUrl = builder.Configuration["HuggingFace:ModelUrl"];
+var huggingFaceKey = builder.Configuration["HuggingFace:ApiKey"];
+
+builder.Services.AddHttpClient<HuggingFaceClient>(client =>
+{
+    client.BaseAddress = new Uri(huggingFaceUrl!);
+    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {huggingFaceKey}");
+});
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
