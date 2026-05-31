65266c83f8ac6af4374192014794c52fcc16d956
Hande Bengü <hande.bengu@chasacademy.se>
2026-05-31 15:37:02 +0200

    feat: add exception middleware to LLMProxyAPI

---
+++ ContentAPI/Exceptions/AiServiceException.cs
@@ -0,0 +1,13 @@
+﻿namespace ContentAPI.Exceptions
+{
+    public class AiServiceException : Exception
+    {
+        public int StatusCode { get; }
+
+        public AiServiceException(string message, int statusCode = 502)
+            : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
